26e675d6d1cc7cdc538a772c6fb3537777748626
Mynameserika <erika.cabrera15@aol.com>
2017-03-20 20:59:06 +0000

    my calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,41 +5,58 @@
 
 function divide(x,y)
 {
-	//Write your code below this line
-	
+if(y===0){
+return "0";
+}
+var dividison = x / y;
+return dividison;
+
 }
 
 function multiply(x,y)
 {
-	
+    var multipltion = x * y;
+    return multipltion;
 }
 
 function subtract(x,y)
 {
-	
+    var subraction = x- y;
+return subraction;
 }
+
 
 function add(x,y)
 {
-	
+return x + y;
 }
 
+
+
+function triplenumber(x) {
+    return x * 3;
+}
 function power(x,y)
 {
-	
+	var powerful = Math.pow(x,y)
+	return powerful
 }
 
 function square_root(x)
 {
-	
+var square=Math.sqrt(x)	
+return square;
 }
 
 function logBase10(x)
 {
-	
+	var log=Math.log10(x)
+	return log;
 }
 
 function generateRandomNumber()
 {
-	
+var generate=Math.random()	
+
+return generate;
 }
